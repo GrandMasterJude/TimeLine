bcb4410b022a7ba24e70a9ec9f92cfc2301d5374
Jude <grandmasterjude@gmail.com>
2016-11-02 10:52:29 -0500

    Trying to sync.

---
+++ dbo.Person.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Person] (
+    [Id]    INT            IDENTITY (1, 1) NOT NULL,
+    [Name]  NVARCHAR (200) NOT NULL,
+    [DOB]   NVARCHAR (100) NULL,
+    [DOD]   NVARCHAR (100) NULL,
+    [Photo] IMAGE          NULL,
+    [StyleID] INT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC), 
+    CONSTRAINT [FK_Person_Style] FOREIGN KEY ([StyleID]) REFERENCES [Style]([Id])
+);
+
+
+GO
+
+CREATE FULLTEXT INDEX ON [dbo].[Person] ([Name]) KEY INDEX [Person_Name_Index] ON [fulltext_catalog_name] WITH CHANGE_TRACKING AUTO
